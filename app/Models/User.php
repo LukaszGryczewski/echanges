78040439a49daef44aebc0e5bed38c0662c20f39
Lukasz Gryczewski <luka.gryczewski@gmail.com>
2023-07-24 21:58:49 +0200

    Create model Order + migration + Relationships OneToMany between User and Order

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,4 +72,9 @@ class User extends Authenticatable
     public function products(){
         return $this->hasMany(Product::class);
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
